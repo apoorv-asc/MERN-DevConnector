7ba1c34e4fe2341ebcb55fa984e57d3e453903e0
apoorv-asc <apoorvsingh.chauhan2019@vitstudent.ac.in>
2021-06-28 20:15:45 +0530

    Load User & Set Auth

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,11 +3,12 @@ import {connect} from 'react-redux';
 import { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types'
 
 // import axios from 'axios';
 
-const Register = ({setAlert}) => {
+const Register = ({setAlert,register}) => {
     const [formData, setFromData] = useState({
         name:'',
         email:'',
@@ -21,10 +22,9 @@ const Register = ({setAlert}) => {
     const onSubmit = async e =>{
         e.preventDefault();
         if(password !==password2){
-            setAlert('Passwords don\'t match','danger');
+          setAlert('Passwords don\'t match','danger');
         }else{
-
-            console.log("Success");
+          register({name,email,password});
         }
     }
 
@@ -40,7 +40,7 @@ const Register = ({setAlert}) => {
                 name="name" 
                 value={name}
                 onChange={e => onChange(e)} 
-                required 
+                // required 
             />
           </div>
           <div className="form-group">
@@ -61,7 +61,7 @@ const Register = ({setAlert}) => {
               type="password"
               placeholder="Password"
               name="password"
-              minLength="6"
+              // minLength="6"
               value={password}
               onChange={e => onChange(e)} 
             />
@@ -71,7 +71,7 @@ const Register = ({setAlert}) => {
               type="password"
               placeholder="Confirm Password"
               name="password2"
-              minLength="6"
+              // minLength="6"
               value={password2}
               onChange={e => onChange(e)} 
             />
@@ -90,8 +90,8 @@ const Register = ({setAlert}) => {
 }
 
 Register.propTypes = {
-  setAlert:PropTypes.func.isRequired
+  setAlert:PropTypes.func.isRequired,
+  register:PropTypes.func.isRequired
 }
 
-{/* Send Redux's info along with export */}
-export default connect(null,{setAlert})(Register);
+export default connect(null,{setAlert,register})(Register);
